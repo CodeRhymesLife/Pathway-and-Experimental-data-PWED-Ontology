1ee46923656cad8cc410d005157c4ac0d43cd442
Ryan James <rcjames1004@gmail.com>
2015-03-10 14:39:15 -0700

    Parse data from array express json

--- arrayExpressDataExtraction.py
+++ arrayExpressDataExtraction.py
@@ -9,13 +9,33 @@ import json
 
 pmid_list = ["http://www.ebi.ac.uk/arrayexpress/json/v2/experiments?pmid=21057493"]
 
+class Publication:
+	pubmedId = 0;
+	title = "";
+	authors = [];
+	
+	def __init__ (self, json):
+		self.pubmedId = json['accession'];
+		self.title = json['title'];
+		self.authors = json['authors'].split(',');
+		
+	def toString (self):
+		return "id: " + str(self.pubmedId) + ", title: " + self.title + ", authors: " + ", ".join(self.authors); 
+
 for query in pmid_list:
     pmid = query[-8:]
     pmid_req = Request(query)
     pmid_res = urlopen(pmid_req)
-    pmid_txt = json.loads(pmid_res.read().decode('utf8'))
-    exp = pmid_txt['experiments']['experiment']
+    queryResultJson = json.loads(pmid_res.read().decode('utf8'))
+    exp = queryResultJson['experiments']['experiment']
     print (":pmid_"+pmid+" rdf:type :Publication, :NamedIndividual; :hasid \""+pmid+"\"^^xsd:string.")
-    
+    species = exp['species'];
+    id = exp['miamescores'];
+    name = exp['name'];
+    year = exp['releasedate'];
+    description = exp['description']['text'];
+    publication = Publication(exp['bibliography']);
+    print("Publication -> " + publication.toString());
+	
 	# prase relevant data
 	# encode in ontology
