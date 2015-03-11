464d6bff56cd5c42c08d834967b20677e56583bd
Ryan James <rcjames1004@gmail.com>
2015-03-10 23:38:06 -0700

    Get array express and publication data
    1) Updated the python script to extract array express data
    2) Created a new ontology with data

--- arrayExpressDataExtraction.py
+++ arrayExpressDataExtraction.py
@@ -9,33 +9,57 @@ import json
 
 pmid_list = ["http://www.ebi.ac.uk/arrayexpress/json/v2/experiments?pmid=21057493"]
 
-class Publication:
-	pubmedId = 0;
-	title = "";
-	authors = [];
+class Entity:
+	json = {};
 	
 	def __init__ (self, json):
-		self.pubmedId = json['accession'];
-		self.title = json['title'];
-		self.authors = json['authors'].split(',');
+		self.json = json;
+
+class Publication(Entity):
+	All = {};
+	
+	def __init__ (self, json):
+		super(Publication, self).__init__(json);
+		Publication.All[self.json['accession']] = self;
 		
 	def toString (self):
-		return "id: " + str(self.pubmedId) + ", title: " + self.title + ", authors: " + ", ".join(self.authors); 
+		authorsString = "";
+		for author in self.json['authors'].split(','):
+			authorsString += "\t:author \"" + author.strip() + "\"^^xsd:string ;\n";
+		
+		return	(self.getId() + " rdf:type :Publication , :NamedIndividual ;\n"
+				"" + authorsString + ""
+				"\t:title \"" + self.json['title'] + "\"^^xsd:string .");
+				
+	def getId (self):
+		return ":pub_" + str(self.json['accession']);
+		
+class Experiment(Entity):
+	Species = {"Homo sapiens": ":homo_sapien"};
 
+	def toString (self):
+		publication = Publication(self.json['bibliography'])
+	
+		return (":exp_" + self.json['accession'] + " rdf:type :Experiment , :NamedIndividual ;\n"
+				"\t:species " + Experiment.Species[self.json['species']] + " ;\n"
+				"\t:name \"" + self.json['name'] + "\"^^xsd:string ;\n"
+				"\t:year \"" + self.json['releasedate'] + "\"^^xsd:string ;\n"
+				"\t:publication " + publication.getId() + " ;\n"
+				"\t:description \"" + self.json['description']['text'] + "\"^^xsd:string .");
+				
+	
+experiments = [];
 for query in pmid_list:
     pmid = query[-8:]
     pmid_req = Request(query)
     pmid_res = urlopen(pmid_req)
     queryResultJson = json.loads(pmid_res.read().decode('utf8'))
-    exp = queryResultJson['experiments']['experiment']
-    print (":pmid_"+pmid+" rdf:type :Publication, :NamedIndividual; :hasid \""+pmid+"\"^^xsd:string.")
-    species = exp['species'];
-    id = exp['miamescores'];
-    name = exp['name'];
-    year = exp['releasedate'];
-    description = exp['description']['text'];
-    publication = Publication(exp['bibliography']);
-    print("Publication -> " + publication.toString());
+    experiments.append(Experiment(queryResultJson['experiments']['experiment']));
 	
-	# prase relevant data
-	# encode in ontology
+print ("Experiments:\n\n")
+for exp in experiments:
+	print (exp.toString() + "\n\n")
+
+print ("\n\n\n\nPublications:\n\n")	
+for id, pub in Publication.All.items():
+	print (pub.toString() + "\n\n")
